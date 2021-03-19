44a98f209d7615fbdce1dc1391a02587d2ec7cd8
cddorz <2878153350@qq.com>
2021-03-19 18:47:46 +0800

    springboot工具模板

---
+++ src/main/java/comhly/ToolModuleApplication.java
@@ -0,0 +1,13 @@
+package comhly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolModuleApplication.class, args);
+    }
+
+}
